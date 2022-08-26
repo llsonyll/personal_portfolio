c795b2922b1fe161ebeae71b96d2f651f81418a2
llsonyll <sony_s07@hotmail.es>
2022-08-25 19:15:53 -0500

    feat: personal description updated

--- src/pages/technoMode/data/personal.js
+++ src/pages/technoMode/data/personal.js
@@ -4,7 +4,7 @@ export const me = {
     fatherLastName: 'Rojas',
     motherLastName: 'Baca',
     fullName: 'Jairo Josep Rojas Baca',
-    description: "I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building accessible, human-centered products at Upstatement.",
+    description: "I'm a web and mobile developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on get a competitive job, that allows me improve my skills and become a senior developer.",
     aboutParagraphs: ["Hello! My name is Josep and I enjoy creating things that live on the internet. My interest in web development started back in 2021 when I decided to try developing some trickiy projects for collegue - turns out researching and deploying some projects that taught me a lot about HTML & CSS and more stuff!", "Fast-forward to today, and I’ve had the privilege of working at an techEd startup. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.", " I also develop a project that covers everything you need to build a web app as social network similar to Facebook and Twitter using React & Node JS."],
     tecnologies: ['React', 'Javascript(ES6+)', 'Typescript', 'SocketIO', 'TailwindCSS', 'Nodemailer', 'Vue', 'Flutter', 'Dart', 'Java(basics)', 'OPP', 'HTML5', 'CSS3', 'C#', 'C(basics)', 'MongoDB', 'Express', 'Mongoose', 'Passport', 'JWT', 'WebRTC', 'FabricJS', 'GraphQL', 'Apollo'],
     experience: [
@@ -47,7 +47,7 @@ export const me = {
     profileURL: 'https://res.cloudinary.com/dr9htpiy2/image/upload/v1661452628/profile_photo_yl6kq8.jpg',
     logoURL: 'https://res.cloudinary.com/dr9htpiy2/image/upload/v1661452636/NiceLogo_kfwfie.png',
     phone: '950368711',
-    contactQuoute: "Currently I'm looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!",
+    contactQuoute: "Currently I'm looking for new opportunities so my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!",
     resumeUrl: 'https://drive.google.com/file/d/1E8Jf7upzrJ96vNjHD_6CR9xhXnlnThuM/view?usp=sharing',
     gitHubProfile: 'https://github.com/llsonyll',
     linkedInProfile: 'https://www.linkedin.com/in/josep-jairo-rojas-baca-34975a1bb/',
